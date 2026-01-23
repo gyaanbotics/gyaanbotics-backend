b17fc12ed9d5796963fa67563eeb29cd985df557
gyaanbotics <gyaanbotics@gmail.com>
2026-01-23 15:02:27 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const adminSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+  email: String,
+  password: String
 });
 
 module.exports = mongoose.model("Admin", adminSchema);
