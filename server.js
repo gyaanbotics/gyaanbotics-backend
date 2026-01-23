0d7adaa45b5ba81547b6d2a959f9f182ef836861
gyaanbotics <gyaanbotics@gmail.com>
2026-01-23 15:13:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,8 +39,3 @@ app.use("/contact/enquiry", enquiryRoutes);
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-const bcrypt = require("bcryptjs");
-
-bcrypt.hash("admin123", 10).then(hash => {
-  console.log("NEW ADMIN HASH:", hash);
-});
