cbc6da70cf0e0373657a433e660d491ca8a6784d
gyaanbotics <gyaanbotics@gmail.com>
2026-01-23 12:30:22 +0530

    Update Enquiry.js

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const EnquirySchema = new mongoose.Schema({
-  name: String,
-  school: String,
-  email: String,
-  phone: String,
-  message: String,
-  createdAt: { type: Date, default: Date.now }
-});
+const enquirySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Enquiry", EnquirySchema);
+module.exports = mongoose.model("Enquiry", enquirySchema);
