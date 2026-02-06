4943f83a18b49f26e25b9364187f144be5de489a
gyaanbotics <gyaanbotics@gmail.com>
2026-02-06 11:52:16 +0530

    Update Enquiry.js

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -4,6 +4,7 @@ const enquirySchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    phone: { type: String, required: true },
     organization: String,
     message: { type: String, required: true },
   },
